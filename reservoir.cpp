8f778950c77b49d6373fafb0cf5fbb26b9f30c99
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-12 22:01:21 -0400

    created file

--- reservoir.cpp
+++ reservoir.cpp
@@ -13,12 +13,12 @@ double get_east_storage(std::string date)
 		exit(1);
 	}
 
-	double  eastSt, eastEl, westSt, westEl;
+	double  eastSt;
 	std::string d = date;
 	std::string junk = "";
 	getline(fin, junk);
 	
-	while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) { 
+	while(fin >> date >> eastSt) { 
 		fin.ignore(INT_MAX, '\n');
 		if(date == d)
 		{
@@ -29,4 +29,48 @@ double get_east_storage(std::string date)
 	std::cout << "East Basin Storage: " << eastSt << " billion gallons" << std::endl;
 	
 	return 0;
+}
+
+double get_min_east()
+{
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+	if (fin.fail()) {
+		std::cerr << "File cannot be opened for reading." << std::endl;
+		exit(1);
+	}
+		double date, eastSt, min;
+		
+		std::string junk = "";
+		getline(fin, junk);
+
+		while(fin >> date >> eastSt) {
+			fin.ignore(INT_MAX, '\n');
+			if (eastSt < min)
+			{
+				std::cout << min;
+				min = eastSt;
+			}
+		}
+		
+		std::cout << "Minimum storage in East basin: " << min << " billion gallons" << std::endl;
+		return 0;
+}
+
+double get_max_east()
+{
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+        if (fin.fail()) {
+		std::cerr << "File cannot be opened for reading." << std::endl;
+		exit(1);
+	}
+		double date, eastSt, min;
+
+		std::string junk = "";
+		getline(fin, junk);
+
+		while(fin >> date >> eastSt) {
+			fin.ignore(INT_MAX, '\n');
+			if (eastSt > min){													min = eastSt;															}
+		}
+													                		std::cout << "Minimum storage in East basin: " << min << " billion gallons" << std::endl;																                return 0;
 }
