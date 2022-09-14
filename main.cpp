e3da016e87466af25141773c32da58842af0e41c
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-14 12:27:21 -0400

    added compare

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ int main()
 	get_east_storage("05/20/2018");  
 	get_min_east();
 	get_max_east();
+	
+	compare_basins("09/13/2018");
+	compare_basins("01/08/2018");
+	compare_basins("09/17/2018");
 
 	return 0;
 }
